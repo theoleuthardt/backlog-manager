af61b65511ee20d392670f99c945e85a753fa310
theoleuthardt <theoleuthardt12@gmail.com>
2024-11-14 23:54:02 +0100

    nextjs base structure done!

---
+++ src/db/queries/update.ts
@@ -0,0 +1,7 @@
+import { eq } from 'drizzle-orm';
+import { db } from '@/db';
+import { SelectPost, postsTable } from '../schema';
+
+export async function updatePost(id: SelectPost['id'], data: Partial<Omit<SelectPost, 'id'>>) {
+    await db.update(postsTable).set(data).where(eq(postsTable.id, id));
+}
