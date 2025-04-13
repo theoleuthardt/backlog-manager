09446a31c5876f97a8590794f204f2ae0c9f9bcb
theoleuthardt <theoleuthardt12@gmail.com>
2025-04-13 20:44:03 +0200

    feat: prettier init

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
+    <a className="" href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
