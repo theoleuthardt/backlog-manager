e7a283dbffff07418b3c022d5d6c622777623e6d
theoleuthardt <theoleuthardt12@gmail.com>
2025-05-13 12:10:03 +0200

    feat: add trpc to base structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
-import "~/styles/globals.css";
+import "../styles/globals.css";
 
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
+
+import { TRPCReactProvider } from "../trpc/react";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -19,7 +21,9 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${geist.variable}`}>
-      <body>{children}</body>
+      <body>
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+      </body>
     </html>
   );
 }
