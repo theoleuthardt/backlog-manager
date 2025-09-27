c9ec4f79d39980439ef474a89d70f042cf06a9c8
theoleuthardt <theoleuthardt12@gmail.com>
2025-09-27 15:36:43 +0200

    fix: use index.ts for component folder exports

--- src/app/_components/index.ts
+++ src/app/_components/index.ts
@@ -2,6 +2,20 @@ import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
 import { ScrollSection } from "./ScrollSection";
 import { Features } from "./Features";
-import { BacklogEntry } from "components/BacklogEntry";
+import { BacklogEntry } from "./BacklogEntry";
+import { CustomDropdownMenu } from "./CustomDropdownMenu";
+import { GameImage } from "./GameImage";
+import { SearchBar } from "./SearchBar";
+import { UniSlider } from "./UniSlider";
 
-export { Navbar, Footer, ScrollSection, Features, BacklogEntry };
+export {
+  Navbar,
+  Footer,
+  ScrollSection,
+  Features,
+  BacklogEntry,
+  CustomDropdownMenu,
+  GameImage,
+  SearchBar,
+  UniSlider,
+};
