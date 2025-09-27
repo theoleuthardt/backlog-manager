045ac5b6c35d9347531b4dfc4b34cd49cab1309d
theoleuthardt <theoleuthardt12@gmail.com>
2025-09-27 14:04:19 +0200

    feat: SearchBar component with props and, english placeholder and className injection

--- src/app/_components/SearchBar.tsx
+++ src/app/_components/SearchBar.tsx
@@ -2,16 +2,21 @@ import React from "react";
 import { Input } from "~/components/ui/input";
 import Image from "next/image";
 
-export const SearchBar = () => {
+export interface SearchBarProps {
+  placeholder?: string;
+  className?: string;
+}
+
+export const SearchBar = (props: SearchBarProps) => {
   return (
-    <div className="relative mb-4 w-full max-w-sm">
+    <div className={`relative mb-4 w-full max-w-sm ${props.className}`}>
       <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
         <Image src="/search.png" alt="Search" width={20} height={20} />
       </div>
       <Input
         type="search"
-        placeholder="Suchen..."
-        className="w-full border-2 border-blue-700 pl-10 focus:ring-2 focus:ring-blue-700"
+        placeholder="Seach..."
+        className="w-full border-2 pl-10"
       />
     </div>
   );
