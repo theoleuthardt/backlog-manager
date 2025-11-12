8dc15745db7584bdd48bc27eff70969a21a7047b
laura194 <voss.2000@gmx.de>
2025-11-12 17:01:04 +0100

    refactor: Simplify RootLayout component by removing unused font import and HTML structure

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,7 +1,6 @@
 import "../../styles/globals.css";
 
 import { type Metadata } from "next";
-import { Geist } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
 
@@ -11,19 +10,8 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-const geist = Geist({
-  subsets: ["latin"],
-  variable: "--font-geist-sans",
-});
-
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en" className={`${geist.variable}`}>
-      <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
-      </body>
-    </html>
-  );
+  return <TRPCReactProvider>{children}</TRPCReactProvider>;
 }
