aa1534c7a051d22775926fd59b0b1b2ca48d0003
laura194 <voss.2000@gmx.de>
2025-11-12 16:59:59 +0100

    feat: Implement middleware for protecting dashboard route (and other routes in the future)

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { auth } from "./server/auth";
+
+const protectedPaths = ["/dashboard"];
+
+export default async function middleware(request: NextRequest) {
+  const session = await auth();
+  const {pathname} = request.nextUrl;
+  const isProtectedPath = protectedPaths.some((route) => pathname.startsWith(route));
+
+  if (isProtectedPath && !session?.user) {
+    const loginUrl = new URL("/login", request.url);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  return NextResponse.next();
+}
