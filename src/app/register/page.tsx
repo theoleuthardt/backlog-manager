34b989d6e86941f57bf6f26d89cdfb01cf5bc178
laura194 <voss.2000@gmx.de>
2025-11-15 14:41:44 +0100

    added registration page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,14 @@
+import RegisterForm from "../_components/RegisterForm";
+
+export default function RegisterPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-black to-blue-900 text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 text-white">
+        <h1 className="mb-6 text-5xl font-extrabold text-white sm:text-[5rem]">
+          Create Account
+        </h1>
+        <RegisterForm />
+      </div>
+    </main>
+  );
+}
