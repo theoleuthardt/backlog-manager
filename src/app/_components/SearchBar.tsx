f72b473892c7384e84158e0399cbbc6114797fac
theoleuthardt <theoleuthardt12@gmail.com>
2025-11-17 20:44:50 +0100

    feat: boolean for externally deciding if the icon should be used in the searchbar

--- src/app/_components/SearchBar.tsx
+++ src/app/_components/SearchBar.tsx
@@ -8,19 +8,22 @@ export interface SearchBarProps {
   className?: string;
   onInput?: Function;
   ref?: React.RefObject<HTMLInputElement>;
+  useIcon: boolean;
 }
 
 export const SearchBar = (props: SearchBarProps) => {
   return (
     <div className={`relative mb-4 w-full max-w-sm ${props.className}`}>
-      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-        <Image src="/search.png" alt="Search" width={20} height={20} />
-      </div>
+      {props.useIcon && (
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+          <Image src="/search.png" alt="Search" width={20} height={20} />
+        </div>
+      )}
       <Input
         ref={props.ref}
         type="search"
-        placeholder="Seach..."
-        className="w-full border-2 pl-10"
+        placeholder={props.placeholder || "Search"}
+        className={`w-full border-2 ${props.useIcon ? "pl-10" : ""}`}
         onInput={() => {
           if (props.onInput) props.onInput();
         }}
