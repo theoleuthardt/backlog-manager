e9b1007ce363e067c779ffcc9d5acb3cc51e01d7
AuriomTex <domenik@ronaldwilhelm.de>
2025-11-18 12:04:00 +0100

    feat: created HowLongToBeat API Functions
    - added simple Tests for functions
    - Moved Files to Integrations folder
    - added SearchGame and GetGameByID
    - created interfaces for API Output

---
+++ src/server/integrations/types/index.ts
@@ -0,0 +1,16 @@
+export interface HltbSearchResultItem {
+    id: number;
+    hltbId: number;
+    title: string;
+    imageUrl: string;
+    steamAppId: number | null;
+    gogAppId: number | null;
+    mainStory: number;
+    mainStoryWithExtras: number;
+    completionist: number;
+    lastUpdatedAt: string;
+}
+
+export type HltbSearchResult = HltbSearchResultItem[];
+
+export type HltbGame = HltbSearchResultItem;
--- test/howLongToBeat.test.ts
+++ test/howLongToBeat.test.ts
@@ -1,12 +1,18 @@
 import { describe, it, expect} from 'vitest'
-import { SearchGame} from "~/server/api/HowLongToBeat/howLongToBeat";
+import {GetGameByID, SearchGame} from "~/server/integrations/howlongtobeat/howLongToBeat";
 
 describe('Fortnite', () => {
 
         it('Gooby', async () => {
-            const result = await SearchGame("For")
+            const result = await SearchGame("Fortnite")
             console.log(result)
-            expect(result).toBe("100% Complete!")
+            expect(result.at(0)?.title).toBe("Fortnite")
+        })
+
+        it('Für', async () => {
+            const result = await GetGameByID(2224)
+            console.log(result)
+            expect(result.hltbId).toBe(2224)
         })
 
     })
