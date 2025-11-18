f25b1c2cd8e64f0e09d1b07604474e436372c7eb
theoleuthardt <theoleuthardt12@gmail.com>
2025-11-18 13:12:44 +0100

    feat: game search results dummy data for frontend dev

---
+++ src/constants/gameSearchbarDummyResults.tsx
@@ -0,0 +1,54 @@
+interface SearchResult {
+  title: string;
+  imageUrl: string;
+  steamAppId: number;
+  mainStory: number;
+  mainStoryWithExtras: number;
+  completionist: number;
+}
+
+export const gameSearchbarDummyResults: SearchResult[] = [
+  {
+    title: "SnowRunner",
+    imageUrl: "https://howlongtobeat.com/games/77675_SnowRunner.jpg",
+    steamAppId: 1465360,
+    mainStory: 84.99,
+    mainStoryWithExtras: 155.98,
+    completionist: 236.38,
+  },
+  {
+    title: "Batman: Arkham Asylum GOTY",
+    imageUrl: "https://howlongtobeat.com/games/Batman_Arkham_Asylum_GOTY.jpg",
+    steamAppId: 35140,
+    mainStory: 11.09,
+    mainStoryWithExtras: 15.52,
+    completionist: 25.3,
+  },
+  {
+    title: "Might & Magic: Clash of Heroes - Definitive Edition",
+    imageUrl:
+      "https://howlongtobeat.com/games/127735_Might_&_Magic_Clash_of_Heroes_-_Definitive_Edition.jpg",
+    steamAppId: 2213300,
+    mainStory: 24.7,
+    mainStoryWithExtras: 28.71,
+    completionist: 31.91,
+  },
+  {
+    title: "Kingdom Hearts HD 2.8 Final Chapter Prologue",
+    imageUrl:
+      "https://howlongtobeat.com/games/37863_Kingdom_Hearts_HD_2.8_Final_Chapter_Prologue.jpg",
+    steamAppId: 2552440,
+    mainStory: 19.33,
+    mainStoryWithExtras: 32.67,
+    completionist: 66.82,
+  },
+  {
+    title: "Naruto Shippuden: Ultimate Ninja Storm 4",
+    imageUrl:
+      "https://howlongtobeat.com/games/34139_Naruto_Shippuden_Ultimate_Ninja_Storm_4.jpg",
+    steamAppId: 349040,
+    mainStory: 8.92,
+    mainStoryWithExtras: 19.81,
+    completionist: 39.02,
+  },
+];
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -2,5 +2,12 @@ import { type NavbarLink } from "./navbarLinks";
 import { landingPageNavLinks } from "./navbarLinks";
 import { dashboardNavLinks } from "./navbarLinks";
 import { BacklogEntryDummyData } from "./backlogEntryDummyData";
+import { gameSearchbarDummyResults } from "~/constants/gameSearchbarDummyResults";
 
-export { type NavbarLink, landingPageNavLinks, dashboardNavLinks, BacklogEntryDummyData };
+export {
+  type NavbarLink,
+  landingPageNavLinks,
+  dashboardNavLinks,
+  BacklogEntryDummyData,
+  gameSearchbarDummyResults,
+};
